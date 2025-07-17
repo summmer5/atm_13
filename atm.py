ec901a344b28f3751ce0e222d4754cb01c88e2e4
summmer5 <jisun131200@gmail.com>
2025-07-17 18:59:11 +0900

    ✨ 입금 기능 구현
    사용자가 입금 금액을 입력하고, 잔액을 업데이트 하는 기능 추가

--- atm.py
+++ atm.py
@@ -9,10 +9,13 @@ while True:
     if num == '4': # 4번 종료
         break # 종료
 
-    if num == '1':
-        pass
-
-    if num == '2':
+    if num == '1': # 입금 기능 구현 -> feat/deposit 브랜치에서 작업
+        deposit_amount = int(input('입금할 금액을 입력해 주세요: ')) # str:5000 -> int() -> int:5000
+        balance += deposit_amount # blance(15,000) = blance(10000) + deposit_amount(5000)
+        print(f'입금하신 금액은 {deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.')
+    
+    
+    if num == '2': # 
         pass
     
     if num == '3':
