1731766dfaeea497fcdc2c6d5e5d5401c3f9fc46
summmer5 <jisun131200@gmail.com>
2025-07-18 13:25:17 +0900

    ✨ 영수증 보기 기능 추가
    사용자가 영수증 내역을 확인할 수 있는 기능 추가 및 안내 메시지 작성

--- atm.py
+++ atm.py
@@ -2,9 +2,11 @@
 # 금액은 여러분 마음대로
 
 balance = 10000
+# 초기화는 맨 위에 미리만들기, 특정값이나 빈값을 입력
+receipts = []
 
 while True:
-    num = input("ddd (1.입금, 2.출금, 3.영수증 보기, 4.종료) : ")
+    num = input("사용하실 기능의 번호를 선택해주세요 (1.입금, 2.출금, 3.영수증 보기, 4.종료) : ")
 
     if num == '4': # 4번 종료
         break # 종료
@@ -12,17 +14,30 @@ while True:
     if num == '1': # 입금 기능 구현 -> feat/deposit 브랜치에서 작업
         deposit_amount = int(input('입금할 금액을 입력해주세요: ')) # str:5000 -> int() -> int:5000
         balance += deposit_amount # blance(15,000) = blance(10000) + deposit_amount(5000)
+        # 입금 계산 후 영수증 자리 출력 전, 후
         print(f'입금하신 금액은 {deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.')
-    
+        # 입금 계산 후 영수증 자리 출력 전, 후
+        # 보안 -> '입금', deposit_amount -> 튜플로 만들기
+        receipts.append(('입금', deposit_amount, balance))
+
+
     
     if num == '2': # 
-       withdraw_amount = int(input('출금할 금액을 입력해주세요: '))
-       withdraw_amount = min(balance, withdraw_amount)
-       balance -= withdraw_amount
-       print(f'출금하신 금액은 {withdraw_amount}원이고, 현재 잔액은 {balance}원 입니다.')
-    
+        withdraw_amount = int(input('출금할 금액을 입력해주세요: '))
+        withdraw_amount = min(balance, withdraw_amount)
+        balance -= withdraw_amount
+        print(f'출금하신 금액은 {withdraw_amount}원이고, 현재 잔액은 {balance}원 입니다.')
+        receipts.append(('출금', withdraw_amount, balance))
+
+
     if num == '3':
-        pass
+        if receipts:
+            for i in receipts:
+                print(f"{i[0]}: {i[1]}원 | 잔액: {i[2]}")
+        else:
+            print(f"영수증 내역이 없습니다.")
+
+
 # 코드는 위에서부터 아래로 실행
 
 
